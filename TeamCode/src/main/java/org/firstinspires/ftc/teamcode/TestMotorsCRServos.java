717a6b25608585916a8eccf31448a4bd0ec91e91
Arshad Anas <arshadanas900+github@gmail.com>
2026-01-22 20:29:38 -0500

    remove unused import statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotorsCRServos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotorsCRServos.java
@@ -9,7 +9,6 @@ import static org.firstinspires.ftc.teamcode.Wrap.wrap;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(group = "Single mechanism test")
