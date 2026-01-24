866d0793dd38ec68fa3413cb5212ce1676893519
Arshad Anas <arshadanas900+github@gmail.com>
2026-01-24 12:11:42 -0500

    add rangefinder classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/mechanismtest/ConfigureLaserRangefinder.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.opmode.mechanismtest;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystem.utility.sensor.LaserRangefinder;
+
+@TeleOp
+public class ConfigureLaserRangefinder extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        LaserRangefinder lrf = new LaserRangefinder(hardwareMap.get(RevColorSensorV3.class, "Laser"));
+        telemetry.addData("Pin0", lrf.getPin0Mode());
+        telemetry.addData("Pin1", lrf.getPin1Mode());
+        telemetry.addData("Distance Mode", lrf.getDistanceMode().name());
+        telemetry.addData("Timing [Budget, Period]", java.util.Arrays.toString(lrf.getTiming()));
+        telemetry.addData("ROI", java.util.Arrays.toString(lrf.getROI()));
+        telemetry.addData("Optical Center", java.util.Arrays.toString(lrf.getOpticalCenter()));
+        telemetry.update();
+        waitForStart();
+        /* <configuration code> */
+    }
+}
+
