707b984282c19b8fa7de4c377566830c87dc817e
Arshad Anas <arshadanas900+github@gmail.com>
2026-01-24 21:14:03 -0500

    set crf names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/singlemechtest/ConfigureColorRangefinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/singlemechtest/ConfigureColorRangefinder.java
@@ -11,15 +11,21 @@ import org.firstinspires.ftc.teamcode.subsystem.utility.sensor.ColorRangefinder;
 public class ConfigureColorRangefinder extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        ColorRangefinder crf = new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "Color"));
+        ColorRangefinder[] crfs = {
+                new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "Color 1")),
+                new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "Color 2")),
+        };
         waitForStart();
         /* Using this example configuration, you can detect both artifact colors based on which pin is reading true:
             pin0 --> purple
             pin1 --> green */
-        crf.setPin0Digital(ColorRangefinder.DigitalMode.HSV, 160 / 360.0 * 255, 190 / 360.0 * 255); // purple
-        crf.setPin0DigitalMaxDistance(ColorRangefinder.DigitalMode.HSV, 10); // 10mm or closer requirement
-        crf.setPin1Digital(ColorRangefinder.DigitalMode.HSV, 110 / 360.0 * 255, 140 / 360.0 * 255); // green
-        crf.setPin1DigitalMaxDistance(ColorRangefinder.DigitalMode.HSV, 10); // 10mm or closer requirement
+        for (ColorRangefinder crf : crfs) {
+            crf.setPin0Digital(ColorRangefinder.DigitalMode.HSV, 160 / 360.0 * 255, 190 / 360.0 * 255); // purple
+            crf.setPin0DigitalMaxDistance(ColorRangefinder.DigitalMode.HSV, 10); // 10mm or closer
+
+            crf.setPin1Digital(ColorRangefinder.DigitalMode.HSV, 110 / 360.0 * 255, 140 / 360.0 * 255); // green
+            crf.setPin1DigitalMaxDistance(ColorRangefinder.DigitalMode.HSV, 10); // 10mm or closer requirement
+        }
     }
 }
 
