43e254aec2d7aa84ff7ae34c6fe4a6e6f5773a23
Arshad Anas <arshadanas900+github@gmail.com>
2026-01-24 22:31:31 -0500

    fix config name for i2c colors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/singlemechtest/ConfigureColorRangefinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/singlemechtest/ConfigureColorRangefinder.java
@@ -18,8 +18,8 @@ public class ConfigureColorRangefinder extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         ColorRangefinder[] crfs = {
-                new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "Color 1")),
-                new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "Color 2")),
+                new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "color 1")),
+                new ColorRangefinder(hardwareMap.get(RevColorSensorV3.class, "color 2")),
         };
         waitForStart();
         /* Using this example configuration, you can detect both artifact colors based on which pin is reading true:
