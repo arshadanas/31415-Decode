4bc141440d6c248ade40b632fc1b7321742e886e
Arshad Anas <arshadanas900+github@gmail.com>
2026-01-29 21:20:09 -0500

    remove unused methods from CachedSimpelServo
    and make non-final

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/utility/cachedhardware/CachedSimpleServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/utility/cachedhardware/CachedSimpleServo.java
@@ -3,24 +3,10 @@ package org.firstinspires.ftc.teamcode.subsystem.utility.cachedhardware;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
-public final class CachedSimpleServo extends SimpleServo {
-
-    public CachedSimpleServo(HardwareMap hw, String servoName, double minAngle, double maxAngle, AngleUnit angleUnit) {
-        super(hw, servoName, minAngle, maxAngle, angleUnit);
-    }
+public class CachedSimpleServo extends SimpleServo {
 
     public CachedSimpleServo(HardwareMap hw, String servoName, double minDegree, double maxDegree) {
         super(hw, servoName, minDegree, maxDegree);
-    }
-
-    public static CachedSimpleServo getAxon(HardwareMap hardwareMap, String name) {
-        return new CachedSimpleServo(hardwareMap, name, 0, 355);
-    }
-
-    public static CachedSimpleServo getGBServo(HardwareMap hardwareMap, String name) {
-        return new CachedSimpleServo(hardwareMap, name, 0, 280);
     }
 
     public CachedSimpleServo reversed() {
