9ac769a996208a6dff06bb728a5c3d837b7685f5
Arshad Anas <arshadanas900+github@gmail.com>
2026-01-31 15:53:08 -0500

    LaunchZone.getCurrentZone test cases

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/LaunchZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/LaunchZone.java
@@ -4,6 +4,7 @@ import static org.firstinspires.ftc.teamcode.opmode.Auto.SIZE_FIELD;
 import static org.firstinspires.ftc.teamcode.opmode.Auto.SIZE_TILE;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
 
 import com.pedropathing.geometry.Pose;
 
@@ -66,5 +67,26 @@ public enum LaunchZone {
                 rectangle.intersects(nearTriangle) ? NEAR :
                 rectangle.intersects(farTriangle) ?  FAR :
                                                         NONE;
+    }
+
+    public static void main(String... args) {
+
+        Pose[] poses = {
+                new Pose(103.05710814094775, 75.76184690157962, toRadians(73)), // NONE
+                new Pose(45.04407033129976, 84.21034755347013, toRadians(132)), // NONE
+
+                new Pose(44.480836954507055, 84.58583647133193, toRadians(169)), // NEAR
+                new Pose(123.30743288838414, 111.52020860495438, toRadians(169)), // NEAR
+                new Pose(71.67770668238674, 64.95958279009125, toRadians(180)), // NEAR
+                new Pose(106.78592050246495, 93.30899608865711, toRadians(136)), // NEAR
+
+                new Pose(72.42868451811033, 29.475880052151236, toRadians(180)), // FAR
+                new Pose(52.52777187143498, 14.831812255541072, toRadians(152)), // FAR
+
+                new Pose(89.888919198684, 18.023468057366358, toRadians(136)), // NONE
+        };
+
+        for (Pose pose : poses)
+            System.out.println(LaunchZone.getCurrentZone(pose));
     }
 }
