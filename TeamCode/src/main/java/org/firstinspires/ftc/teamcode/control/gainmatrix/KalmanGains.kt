00be66ff5cdfb5c2d29f13055d0fffd39c8e2adc
Arshad Anas <arshadanas900+github@gmail.com>
2026-01-31 19:38:11 -0500

    comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrix/KalmanGains.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrix/KalmanGains.kt
@@ -1,6 +1,12 @@
 package org.firstinspires.ftc.teamcode.control.gainmatrix
 
 data class KalmanGains @JvmOverloads constructor(
+    /**
+     * model covariance
+     */
     @JvmField var Q: Double = 0.1,
+    /**
+     * sensor covariance
+     */
     @JvmField var R: Double = 0.4,
 )
