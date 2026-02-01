6043976c3836e1b0120c3651dbe5970122376f67
Arshad Anas <arshadanas900+github@gmail.com>
2026-02-01 11:45:43 -0500

    fix TuneTurret setTarget controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/TuneTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/TuneTurret.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import static java.lang.Math.PI;
 import static java.lang.Math.atan2;
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -29,7 +30,16 @@ public final class TuneTurret extends LinearOpMode {
 
             float x = gamepad1.right_stick_x, y = gamepad1.right_stick_y;
             if (x*x + y*y >= 0.64)
-                turret.setTarget(atan2(x, y));
+                turret.setTarget(PI + atan2(x, y));
+
+            if (gamepad1.dpadUpWasPressed())
+                turret.setTarget(0);
+            else if (gamepad1.dpadLeftWasPressed())
+                turret.setTarget(PI/2);
+            else if (gamepad1.dpadDownWasPressed())
+                turret.setTarget(PI);
+            else if (gamepad1.dpadRightWasPressed())
+                turret.setTarget(-PI/2);
 
             turret.printTo(telemetry);
             telemetry.update();
