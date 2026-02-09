2db7e6d5a7dc6a83b0e14d2cd7ec0a150a3c82b5
Yousuf Siddiqui <97410861+xzele@users.noreply.github.com>
2026-02-08 21:50:59 -0500

    install photon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/utility/BulkReader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/utility/BulkReader.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystem.utility;
 
+import com.outoftheboxrobotics.photoncore.PhotonCore;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -7,14 +8,22 @@ import java.util.List;
 
 public final class BulkReader {
 
-    private final List<LynxModule> revHubs;
+//    private final List<LynxModule> revHubs;
 
     public BulkReader(HardwareMap hardwareMap) {
-        revHubs = hardwareMap.getAll(LynxModule.class);
-        for (LynxModule hub : revHubs) hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+//        revHubs = hardwareMap.getAll(LynxModule.class);
+//        for (LynxModule hub : revHubs) hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+
+        PhotonCore.CONTROL_HUB.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        PhotonCore.EXPANSION_HUB.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        PhotonCore.experimental.setMaximumParallelCommands(8);
+        PhotonCore.enable();
     }
 
     public void bulkRead() {
-        for (LynxModule hub : revHubs) hub.clearBulkCache();
+
+        PhotonCore.CONTROL_HUB.clearBulkCache();
+        PhotonCore.EXPANSION_HUB.clearBulkCache();
+//        for (LynxModule hub : revHubs) hub.clearBulkCache();
     }
 }
